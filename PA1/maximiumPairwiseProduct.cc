28e270e0e8fe4f83d4e12f0be0abd6b3fc4e3f67
Sam Gomez <samagome@gmail.com>
2025-07-06 21:48:03 -0700

    PA1 (Optional): Maximum Pairwise Product

---
+++ PA1/maximiumPairwiseProduct.cc
@@ -0,0 +1,33 @@
+/**
+ * @author Samantha Gomez
+ * @date 06JUL2025
+ *
+ * @brief Find the maximum pairwise product in a given list.
+ */
+
+#include <iostream>
+
+int main() {
+  int len = 0;
+  long long first_max = 0, second_max = 0;
+
+  std::cin >> len;
+
+  for (int i = 0; i < len; i++) {
+    long long input;
+    bool is_stored = false;
+    std::cin >> input;
+    if (input > first_max) {
+      is_stored = true;
+      second_max = first_max;
+      first_max = input;
+    }
+    if (!is_stored && input > second_max) {
+      second_max = input;
+    }
+  }
+
+  std::cout << first_max * second_max;
+
+  return 0;
+}
